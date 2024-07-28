b33cadb27bf7a0772e0985679b3a03b6ec6286bc
DYLAN JOEL URIBE BLATNIK <a2022112008@estudiantes.upsa.edu.bo>
2024-07-28 00:39:10 -0400

    Addition of CompaniesController.cs

---
+++ Game-Inventory.API/Controllers/CompaniesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+namespace Game_Inventory.API.Controllers
+{
+    public class CompaniesController
+    {
+
+        [HttpGet]
+        public ExternalException GetCompanies(string name)
+        {
+            return new ExternalException();
+        }
+
+        [HttpDelete]
+        public ExternalException DeleteCompanies(string name)
+        {
+            return new ExternalException();
+        }
+
+        [HttpPost]
+        public ExternalException CreateCompanies(string name)
+        {
+            return new ExternalException();
+        }
+
+        [HttpPut]
+        public ExternalException PutCompanies(string name)
+        {
+            return new ExternalException();
+        }
+    }
+}
