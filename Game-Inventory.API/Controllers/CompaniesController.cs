0d05186d375110614490714e3a70aff5e74b66bd
DYLAN JOEL URIBE BLATNIK <a2022112008@estudiantes.upsa.edu.bo>
2024-07-29 14:59:39 -0400

    The corrections from the review comments have been implemented

--- Game-Inventory.API/Controllers/CompaniesController.cs
+++ Game-Inventory.API/Controllers/CompaniesController.cs
@@ -1,33 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using System.Runtime.InteropServices;
 
-namespace Game_Inventory.API.Controllers
+namespace Game_Inventory.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CompaniesController : ControllerBase
 {
-    public class CompaniesController
+    [HttpGet]
+    [Route("/Companies")]
+    [SwaggerOperation(Tags = ["Companies"])]
+    public IActionResult GetCompanies()
     {
+        throw new NotImplementedException();
+    }
 
-        [HttpGet]
-        public ExternalException GetCompanies(string name)
-        {
-            return new ExternalException();
-        }
+    [HttpGet]
+    [Route("/Companies/{id}")]
+    [SwaggerOperation(Tags = ["Companies"])]
+    public IActionResult GetCompaniesById([FromRoute] int id)
+    {
+        throw new NotImplementedException();
+    }
 
-        [HttpDelete]
-        public ExternalException DeleteCompanies(string name)
-        {
-            return new ExternalException();
-        }
+    [HttpPost]
+    [Route("/Companies")]
+    [SwaggerOperation(Tags = ["Companies"])]
+    public IActionResult CreateCompanies([FromBody] string name)
+    {
+        throw new NotImplementedException();
+    }
 
-        [HttpPost]
-        public ExternalException CreateCompanies(string name)
-        {
-            return new ExternalException();
-        }
+    [HttpDelete]
+    [Route("/Companies")]
+    [SwaggerOperation(Tags = ["Companies"])]
+    public IActionResult DeleteCompanies([FromBody] string name)
+    {
+        throw new NotImplementedException();
+    }
 
-        [HttpPut]
-        public ExternalException PutCompanies(string name)
-        {
-            return new ExternalException();
-        }
+    [HttpPut]
+    [Route("/Companies/{id}")]
+    [SwaggerOperation(Tags = ["Companies"])]
+    public IActionResult UpdateCompanies([FromRoute] int id, [FromBody] string body)
+    {
+        throw new NotImplementedException();
     }
 }
