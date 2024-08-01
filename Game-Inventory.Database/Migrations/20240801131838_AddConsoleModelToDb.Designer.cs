dc204e4ccd1b8fdacf4ee2cd69f48d45a15ee94b
DYLAN JOEL URIBE BLATNIK <a2022112008@estudiantes.upsa.edu.bo>
2024-08-01 09:40:16 -0400

    The migrations have been fixed

--- Game-Inventory.Database/Migrations/20240801131838_AddConsoleModelToDb.Designer.cs
+++ Game-Inventory.Database/Migrations/20240801131838_AddConsoleModelToDb.Designer.cs
@@ -3,6 +3,7 @@ using Game_Inventory.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Game_Inventory.Database.Migrations
 {
     [DbContext(typeof(GameStopContext))]
-    partial class GameStopContextModelSnapshot : ModelSnapshot
+    [Migration("20240801131838_AddConsoleModelToDb")]
+    partial class AddConsoleModelToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
